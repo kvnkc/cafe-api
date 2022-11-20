44f2b2a7eb97631f7a5d6d80ac0c753303b790b3
kevin cheung <kevin.cheung.kai@gmail.com>
2022-11-20 00:13:55 -0500

    implemented http get request to search for cafe locations

--- main.py
+++ main.py
@@ -57,6 +57,15 @@ def get_all_cafes():
     return jsonify(cafe=[cafe.to_dict() for cafe in cafe_list])
 
 
+@app.route('/search', methods=['GET'])
+def get_cafe_at_location():
+    query_location = request.args.get('loc')
+    cafes = db.session.query(Cafe).filter_by(location=query_location).all()
+    if cafes:
+        return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
+    else:
+        return jsonify(error={'Not Found': 'Sorry, we do not have a cafe at that location'})
+
 # HTTP POST - Create Record
 
 # HTTP PUT/PATCH - Update Record
