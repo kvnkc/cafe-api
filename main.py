945e1e830aad20bc0a7d84d7a10325f19423e17b
kevin cheung <kevin.cheung.kai@gmail.com>
2022-11-19 23:36:39 -0500

    implemented http get request for random cafe

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import random
 
 app = Flask(__name__)
 
-##Connect to Database
+# Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.app_context().push()
 
 
-##Cafe TABLE Configuration
+# Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -23,19 +25,34 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+
+db.create_all()
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
-    
 
-## HTTP GET - Read Record
 
-## HTTP POST - Create Record
+# HTTP GET - Read Record
+@app.route('/random', methods=['GET'])
+def get_random_cafe():
+    random_number = random.randint(1, 21)
+    random_cafe = Cafe.query.filter_by(id=random_number).first()
+    return jsonify(cafe=random_cafe.to_dict())
 
-## HTTP PUT/PATCH - Update Record
+# HTTP POST - Create Record
 
-## HTTP DELETE - Delete Record
+# HTTP PUT/PATCH - Update Record
+
+# HTTP DELETE - Delete Record
 
 
 if __name__ == '__main__':
